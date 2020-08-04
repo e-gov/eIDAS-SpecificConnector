49868f8e04229e031839c7385d6f7e707c1273ba
aarmam <mart.aarma@nortal.com>
2020-08-04 13:07:32 +0300

    TARA2-40 Configuration

--- src/main/java/ee/ria/eidas/connector/specific/config/SpecificConnectorProperties.java
+++ src/main/java/ee/ria/eidas/connector/specific/config/SpecificConnectorProperties.java
@@ -1,50 +1,192 @@
 package ee.ria.eidas.connector.specific.config;
 
-import lombok.Data;
-import lombok.ToString;
+import com.google.common.collect.ImmutableMap;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.opensaml.xmlsec.signature.support.SignatureConstants;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.util.Assert;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import java.util.AbstractMap;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
+import javax.validation.constraints.*;
+import java.util.List;
 
 @ConfigurationProperties(prefix = "eidas.connector")
+@Getter
 @Validated
-@Data
+@AllArgsConstructor
+@ConstructorBinding
 public class SpecificConnectorProperties {
 
-    @NotNull
-    private String appInstanceId;
+    @NotEmpty
+    private final String appInstanceId;
+
+    @NotEmpty
+    private final String specificConnectorRequestUrl;
 
     @Valid
-    private CacheProperties communicationCache = new CacheProperties();
+    private final CacheProperties communicationCache;
 
-    @Data
-    @ToString
-    public static class CacheProperties {
+    @Valid
+    private final Metadata metadata;
 
-        public static final String INCOMING_NODE_REQUESTS_CACHE = "incoming-node-requests-cache";
-        public static final String OUTGOING_NODE_RESPONSES_CACHE = "outgoing-node-responses-cache";
+    private final List<@Valid ServiceProvider> serviceProviders;
 
-        private static Map<String, String> cacheNameMapping = Stream.of(
-                new AbstractMap.SimpleEntry<>(INCOMING_NODE_REQUESTS_CACHE, "specificNodeConnectorRequestCache"),
-                new AbstractMap.SimpleEntry<>(OUTGOING_NODE_RESPONSES_CACHE, "nodeSpecificConnectorResponseCache"))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    @Getter
+    @AllArgsConstructor
+    @ConstructorBinding
+    public static class ServiceProvider {
+
+        @NotEmpty
+        private final String id;
+
+        @NotEmpty
+        @Pattern(regexp = "^https://.*$", message = "must use https protocol")
+        private final String entityId;
+
+        @NotEmpty
+        private final String publicKey;
+
+        @NotEmpty
+        @Pattern(regexp = "^(public|private)$", message = "invalid Service Provider type")
+        private final String type;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @ConstructorBinding
+    public static class Metadata {
+
+        @NotEmpty
+        private final String keyStore;
+
+        @NotEmpty
+        private final String keyStorePassword;
+
+        @NotEmpty
+        private final String keyAlias;
+
+        @NotEmpty
+        private final String keyPassword;
+
+        @NotEmpty
+        private final String entityId;
+
+        @Pattern(regexp = "^(public|private)$", message = "invalid Service Provider type")
+        private final String spType;
+
+        @NotEmpty
+        private final String ssoServiceUrl;
 
         @NotNull
-        private String igniteConfigurationFileLocation;
+        private final Integer validityInDays;
 
-        private String igniteConfigurationBeanName = "igniteSpecificCommunication.cfg";
+        @NotNull
+        @Size(min = 1)
+        private final List<@NotEmpty String> digestMethods;
+
+        @NotNull
+        @Size(min = 1)
+        private final List<@NotEmpty String> signingMethods;
+
+        private final List<@Pattern(regexp = "^[A-Z]{2}$") String> supportedMemberStates;
+
+        @Valid
+        private final List<Attributes> attributes;
+
+        private final String signatureAlgorithm;
+
+        @Valid
+        private final Organization organization;
+
+        @Valid
+        private final Contact supportContact;
+
+        @Valid
+        private final Contact technicalContact;
+
+        public String getSignatureAlgorithm() {
+            return signatureAlgorithm == null ? SignatureConstants.ALGO_ID_SIGNATURE_RSA_SHA512 : signatureAlgorithm;
+        }
+
+        public String getSpType() {
+            return spType == null ? "public" : spType;
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @ConstructorBinding
+    public static class Attributes {
+
+        @NotEmpty
+        private final String name;
+
+        @NotEmpty
+        private final String friendlyName;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @ConstructorBinding
+    public static class Organization {
+
+        @NotEmpty
+        private final String name;
+
+        @NotEmpty
+        private final String displayName;
+
+        @NotEmpty
+        private final String url;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @ConstructorBinding
+    public static class Contact {
+
+        @NotEmpty
+        private final String surname;
+
+        @NotEmpty
+        private final String givenName;
+
+        @NotEmpty
+        private final String company;
+
+        @NotEmpty
+        private final String phone;
+
+        @Email
+        @NotEmpty
+        private final String email;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @ConstructorBinding
+    public static class CacheProperties {
+        public static final String INCOMING_NODE_REQUESTS_CACHE = "incoming-node-requests-cache";
+        public static final String OUTGOING_NODE_RESPONSES_CACHE = "outgoing-node-responses-cache";
+        private static final ImmutableMap<String, String> cacheNameMapping = ImmutableMap.<String, String>builder()
+                .put(INCOMING_NODE_REQUESTS_CACHE, "specificNodeConnectorRequestCache")
+                .put(OUTGOING_NODE_RESPONSES_CACHE, "nodeSpecificConnectorResponseCache")
+                .build();
+
+        @NotNull
+        private final String igniteConfigurationFileLocation;
+        private final String igniteConfigurationBeanName;
 
         public static String getCacheName(String cacheName) {
             Assert.isTrue(cacheNameMapping.containsKey(cacheName), "Cache name mapping is required for " + cacheName + "!");
             return cacheNameMapping.get(cacheName);
+        }
+
+        public String getIgniteConfigurationBeanName() {
+            return igniteConfigurationBeanName == null ? "igniteSpecificCommunication.cfg" : igniteConfigurationBeanName;
         }
     }
 }
