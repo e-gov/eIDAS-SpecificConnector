bae61d579cd69eac479313b843c3dc6cedcf52e3
aarmam <mart.aarma@nortal.com>
2020-09-08 09:19:59 +0300

    Unnecessary configuration

--- src/main/java/ee/ria/eidas/connector/specific/config/SpecificConnectorConfiguration.java
+++ src/main/java/ee/ria/eidas/connector/specific/config/SpecificConnectorConfiguration.java
@@ -63,14 +63,11 @@ public class SpecificConnectorConfiguration implements WebMvcConfigurer {
     public static PropertySourcesPlaceholderConfigurer properties(
             @Value("#{environment.SPECIFIC_CONNECTOR_CONFIG_REPOSITORY}/specificCommunicationDefinitionConnector.xml")
                     String specificCommunicationConfig,
-            @Value("#{environment.SPECIFIC_CONNECTOR_CONFIG_REPOSITORY}/specificConnector.xml")
-                    String specificConnector,
             @Value("#{environment.EIDAS_CONFIG_REPOSITORY}/eidas.xml") String eidasConfig) throws MalformedURLException {
         Assert.isTrue(new File(specificCommunicationConfig).exists(), "Required configuration file not found: " + specificCommunicationConfig);
-        Assert.isTrue(new File(specificConnector).exists(), "Required configuration file not found: " + specificConnector);
         Assert.isTrue(new File(eidasConfig).exists(), "Required configuration file not found: " + eidasConfig);
         PropertySourcesPlaceholderConfigurer ppc = new PropertySourcesPlaceholderConfigurer();
-        ppc.setLocations(new FileUrlResource(specificCommunicationConfig), new FileUrlResource(specificConnector), new FileUrlResource(eidasConfig));
+        ppc.setLocations(new FileUrlResource(specificCommunicationConfig), new FileUrlResource(eidasConfig));
         ppc.setIgnoreUnresolvablePlaceholders(false);
         return ppc;
     }
