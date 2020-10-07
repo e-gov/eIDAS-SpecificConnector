c627b0cf3bd883f58205e565aa22d1882ddf0f8c
aarmam <mart.aarma@nortal.com>
2020-10-07 18:25:48 +0300

    TARA2-40 Add Spring Security and StrictHttpFirewall

---
+++ src/main/java/ee/ria/eidas/connector/specific/config/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package ee.ria.eidas.connector.specific.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers()
+                .frameOptions().deny()
+                .httpStrictTransportSecurity()
+                .includeSubDomains(true)
+                .maxAgeInSeconds(600000);
+    }
+
+    @Override
+    public void configure(WebSecurity webSecurity) {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setUnsafeAllowAnyHttpMethod(true);
+        webSecurity.httpFirewall(firewall);
+    }
+}
