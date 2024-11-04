9f31cb208e7451243fb6dcfb679d264a2ef7ec60
Alar Kvell <alar.kvell@nortal.com>
2024-11-04 15:08:45 +0200

    AUT-1969 Add Elastic APM agent

--- src/main/java/ee/ria/eidas/connector/specific/SpecificConnectorApplication.java
+++ src/main/java/ee/ria/eidas/connector/specific/SpecificConnectorApplication.java
@@ -1,5 +1,6 @@
 package ee.ria.eidas.connector.specific;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
@@ -13,11 +14,13 @@ public class SpecificConnectorApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         Assert.notNull(System.getenv("EIDAS_CONFIG_REPOSITORY"), "Required environment variable EIDAS_CONFIG_REPOSITORY is not set");
         Assert.notNull(System.getenv("SPECIFIC_CONNECTOR_CONFIG_REPOSITORY"), "Required environment variable SPECIFIC_CONNECTOR_CONFIG_REPOSITORY is not set");
+        ElasticApmAttacher.attach();
         SpringApplication.run(SpecificConnectorApplication.class, args);
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        ElasticApmAttacher.attach();
         return application.sources(SpecificConnectorApplication.class).properties("server.error.whitelabel.enabled=false");
     }
 }
